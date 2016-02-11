80fabccf9aaea0db435f13534d4b7349f14068a4
brunomassa <massa_ferreira@hotmail.com>
2016-02-11 17:46:55 +0000

    notification is implemented and working

--- app/src/main/java/com/example/massa/luxvilla/services/notificationservice.java
+++ app/src/main/java/com/example/massa/luxvilla/services/notificationservice.java
@@ -2,11 +2,19 @@ package com.example.massa.luxvilla.services;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.RingtoneManager;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -14,6 +22,8 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.massa.luxvilla.MainActivity;
+import com.example.massa.luxvilla.R;
 import com.example.massa.luxvilla.network.VolleySingleton;
 import com.example.massa.luxvilla.sqlite.BDAdapter;
 import com.example.massa.luxvilla.utils.keys;
@@ -80,6 +90,7 @@ public class notificationservice extends IntentService {
 
 
         if (array!=null||array.length()>0){
+            Boolean newhouses=false;
 
             adapter=new BDAdapter(this);
 
@@ -98,8 +109,9 @@ public class notificationservice extends IntentService {
                     String precsql=adapter.verprecos(id);
                     String infossql=adapter.verinfos(id);
                     if (local.equalsIgnoreCase(locsql) && preco.equalsIgnoreCase(precsql) && info.equalsIgnoreCase(infossql)){
-                        Toast.makeText(this,"IGUAIS",Toast.LENGTH_LONG).show();
+                        //Toast.makeText(this,"IGUAIS",Toast.LENGTH_LONG).show();
                     }else {
+                        newhouses=true;
                         /*long longid=adapter.inserirdados(local,preco,info);
                         if (longid>0){
                             Toast.makeText(this, "Casa adicionada ao modo offline", Toast.LENGTH_LONG).show();
@@ -111,6 +123,36 @@ public class notificationservice extends IntentService {
                 } catch (JSONException e) {
 
                 }
+            }
+
+            if (newhouses){
+                Bitmap imglarge;
+                NotificationCompat.Builder notification= new NotificationCompat.Builder(this)
+                        .setTicker("Novas casas disponiveis")
+                        .setSmallIcon(R.drawable.ic_account_balance_white_24dp)
+                        .setContentTitle("Novas casas disponiveis")
+                        .setContentText("Estão disponiveis novas casas")
+                        .setAutoCancel(true);
+
+                Intent it=new Intent(this, MainActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                notification.setContentIntent(pi);
+                notification.setVibrate(new long[]{1000, 1000, 1000, 1000});
+
+                Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+                notification.setSound(uri);
+
+                notification.setPriority(NotificationCompat.PRIORITY_HIGH);
+                imglarge= BitmapFactory.decodeResource(this.getResources(),
+                        R.drawable.logo);
+                notification.setLargeIcon(imglarge);
+
+                NotificationManager nm=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                nm.notify(0,notification.build());
+            }else {
+
             }
 
         }
