e1109722887dfb5d219acc46218b3ed6baf088c1
brunomassa <massa_ferreira@hotmail.com>
2017-02-24 13:49:30 +0000

    updates in foreground notifications

--- app/src/main/java/com/example/massa/luxvilla/notificacoes/FirebaseMessagingService.java
+++ app/src/main/java/com/example/massa/luxvilla/notificacoes/FirebaseMessagingService.java
@@ -10,7 +10,6 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.example.massa.luxvilla.Actividades.casaactivity;
-import com.example.massa.luxvilla.MainActivity;
 import com.example.massa.luxvilla.R;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -92,6 +91,7 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
                 .setAutoCancel(true)
                 .setColor(getResources().getColor(R.color.colorPrimary))
                 .setSound(defaultSoundUri)
+                .setPriority(2)
                 .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
